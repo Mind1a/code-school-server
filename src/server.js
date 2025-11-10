ec499e45182e4ad0e16b780851bdcb80b76d1c95
Saba-Vardosanidze <sabavardo674@gmail.com>
2025-11-11 02:48:09 +0400

    feature/course-routes-connect-server

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ const dotenv = require('dotenv');
 const { StatusCodes } = require('http-status-codes');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
+const { courseRoutes } = require('./routes/courseRoutes');
 
 dotenv.config();
 
@@ -19,6 +20,7 @@ app.use(
     credentials: true,
   })
 );
+app.use('/api', courseRoutes);
 
 app.use((req, res, next) => {
   const error = new Error('Not Found - ' + req.originalUrl);
